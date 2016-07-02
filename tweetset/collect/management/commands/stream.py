cf5f784d65f08342672803e7ee919a5043daa30e
Michele Colombo <mikexine@gmail.com>
2016-07-02 15:07:06 +0200

    from twython to tweepy

--- tweetset/collect/management/commands/stream.py
+++ tweetset/collect/management/commands/stream.py
@@ -2,10 +2,15 @@ from django.core.management.base import BaseCommand, CommandError
 
 import sys
 import logging
+import json
 import signal
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
 from twython import TwythonStreamer
 from collect.models import Collection, Tweet
 from django.conf import settings
+
 
 class Command(BaseCommand):
     args = 'collection_id'
@@ -29,28 +34,52 @@ class Command(BaseCommand):
 
         collection = Collection.objects.get(pk=args[0])
 
-        class TapStreamer(TwythonStreamer):
-            def on_success(self, data):
-                if 'text' in data:
-                    try:
-                        t, created = Tweet.objects.get_or_create(collection=collection,twitter_id=str(data['id']))
-                        t.data = data
-                        t.save()
-                    except Exception:
+        class MyListener(StreamListener):
+            def on_status(self, status):
+                try:
+                    print "123123412341241234"
+                    print status.id
+                    data = json.dumps(status._json)
+                    print data
+                    t, created = Tweet.objects.get_or_create(collection=collection,twitter_id=str(status.id))
+                    t.data = data
+                    logger.warn(str(t))
+                    t.save()
+                except Exception:
                         exc_type, exc_obj, exc_tb = sys.exc_info()
                         logger.error("Couldn't save a tweet: "+str(exc_obj))
-                if 'limit' in data:
-                    logger.warn("The filtered stream has matched more Tweets than its current rate limit allows it to be delivered.")
-            def on_error(self, status_code, data):
+                # if 'limit' in status:
+                #     logger.warn("The filtered stream has matched more Tweets than its current rate limit allows it to be delivered.")
+            def on_error(self, status):
                 logger.error("Received error code "+str(status_code)+".")
+
+        # class TapStreamer(TwythonStreamer):
+        #     def on_success(self, data):
+        #         if 'text' in data:
+        #             try:
+        #                 t, created = Tweet.objects.get_or_create(collection=collection,twitter_id=str(data['id']))
+        #                 t.data = data
+        #                 t.save()
+        #             except Exception:
+        #                 exc_type, exc_obj, exc_tb = sys.exc_info()
+        #                 logger.error("Couldn't save a tweet: "+str(exc_obj))
+        #         if 'limit' in data:
+        #             logger.warn("The filtered stream has matched more Tweets than its current rate limit allows it to be delivered.")
+        #     def on_error(self, status_code, data):
+        #         logger.error("Received error code "+str(status_code)+".")
 
         social_auth = collection.user.social_auth.get(provider='twitter')
 
-        stream = TapStreamer(settings.SOCIAL_AUTH_TWITTER_KEY, settings.SOCIAL_AUTH_TWITTER_SECRET, social_auth.tokens['oauth_token'], social_auth.tokens['oauth_token_secret'])
+        # stream = TapStreamer(settings.SOCIAL_AUTH_TWITTER_KEY, settings.SOCIAL_AUTH_TWITTER_SECRET, social_auth.tokens['oauth_token'], social_auth.tokens['oauth_token_secret'])
+
+        auth = OAuthHandler(settings.SOCIAL_AUTH_TWITTER_KEY, settings.SOCIAL_AUTH_TWITTER_SECRET)
+        auth.set_access_token(social_auth.tokens['oauth_token'], social_auth.tokens['oauth_token_secret'])
 
         logger.info("Collecting tweets from the streaming API...")
-
+        twitter_stream = Stream(auth, MyListener())
         if collection.follow or collection.track or collection.locations:
-            stream.statuses.filter(follow=collection.follow,track=collection.track,locations=collection.locations)
+            # stream.statuses.filter(follow=collection.follow,track=collection.track,locations=collection.locations)
+            twitter_stream.filter(follow=collection.follow,track=collection.track,locations=collection.locations)
         else:
-            stream.statuses.sample()
+            # stream.statuses.sample()
+            pass
